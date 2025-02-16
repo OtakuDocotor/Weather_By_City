e03462d4655daa72b196bc5b13958adf324dd3e0
Doctor <sokol767676@gmail.com>
2025-02-16 16:50:08 +0300

    Создание и инциализация проекта

---
+++ weather_backend/src/main/java/weather_test/example/weather_test/WeatherTestApplication.java
@@ -0,0 +1,13 @@
+package weather_test.example.weather_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTestApplication.class, args);
+	}
+
+}
---
+++ weather_backend/src/test/java/weather_test/example/weather_test/WeatherTestApplicationTests.java
@@ -0,0 +1,13 @@
+package weather_test.example.weather_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
